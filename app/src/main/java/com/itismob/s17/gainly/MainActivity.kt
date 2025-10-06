25e9b072200f80ede467e668641bd526dbe32425
algen_abagat <144084800+algenabagat@users.noreply.github.com>
2025-10-06 14:01:58 +0800

    initial main page layout

--- app/src/main/java/com/itismob/s17/gainly/MainActivity.kt
+++ app/src/main/java/com/itismob/s17/gainly/MainActivity.kt
@@ -17,31 +17,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            GainlyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
+        setContentView(R.layout.main_page)
+
+}
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GainlyTheme {
-        Greeting("Android")
-    }
-}
