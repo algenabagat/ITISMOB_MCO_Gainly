233678ad343efe7fba68f061bc2e018027c3e74e
michaelaaronluyun <michael_aaron_luyun@dlsu.edu.ph>
2025-10-11 14:35:48 +0800

    fix?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,15 +50,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.recyclerview)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     implementation(libs.androidx.appcompat)
-=======
     implementation(libs.androidx.constraintlayout)
->>>>>>> Stashed changes
-=======
     implementation(libs.androidx.constraintlayout)
->>>>>>> Stashed changes
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
