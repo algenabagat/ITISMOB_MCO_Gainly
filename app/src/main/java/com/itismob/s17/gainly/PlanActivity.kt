d1af05941d67d8bec6948ccdebc9885a21ff079a
EdgarRykel <cody_casem@dlsu.edu.ph>
2025-10-14 16:45:18 +0800

    Created: plan dialog popup - WIP

--- app/src/main/java/com/itismob/s17/gainly/PlanActivity.kt
+++ app/src/main/java/com/itismob/s17/gainly/PlanActivity.kt
@@ -1,7 +1,9 @@
 package com.itismob.s17.gainly
 
+import android.app.Dialog
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageButton
 
 class PlanActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,42 @@ class PlanActivity : BaseActivity() {
     }
 
     private fun setupClickListeners() {
+        val newPlanBtn = findViewById<Button>(R.id.newPlanBtn)
+        val pickTimeBtn = findViewById<Button>(R.id.pickTime)
+        val pickDateBtn = findViewById<Button>(R.id.pickDate)
 
+        newPlanBtn.setOnClickListener {
+            showCreatePlanDialog()
+        }
+
+        pickTimeBtn.setOnClickListener {
+            TimePickerFragment().show(supportFragmentManager, "timePicker")
+        }
+
+        pickDateBtn.setOnClickListener {
+            val newFragment = DatePickerFragment()
+            newFragment.show(supportFragmentManager, "datePicker")
+        }
     }
+
+    private fun showCreatePlanDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.new_plan_popup)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val closeBtn = dialog.findViewById<ImageButton>(R.id.closePlanBtn)
+        val createPlanBtn = dialog.findViewById<Button>(R.id.createPlanBtn)
+
+
+        closeBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        createPlanBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
 }
